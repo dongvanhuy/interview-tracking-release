975711d5aafbb7fe271dc6f6ee3b5f22a13bcd6f
VDC\thuynh63 <quocthang.it247@gmail.com>
2018-08-23 13:46:54 +0700

    test config azure

--- app.js
+++ app.js
@@ -6,27 +6,66 @@
 
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
-var express = require('express');
-var path = require('path');
-
+var express = require("express");
+var path = require("path");
 
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
-var cfenv = require('cfenv');
+var cfenv = require("cfenv");
 
+var serveStatic = require("serve-static-restify");
 // create a new express server
-var app = express();
+//var app = express();
 
 // serve the files out of ./public as our main files
-app.use(express.static(path.resolve(__dirname, '.', 'public')));
-app.get('*', (req, res) => {
-res.sendFile(path.resolve(__dirname, '.', 'public', 'index.html'));
-}); 
+// app.use(express.static(path.resolve(__dirname, ".", "public")));
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, ".", "public", "index.html"));
+// });
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
 
-// start server on the specified port and binding host
-app.listen(appEnv.port, '0.0.0.0', function() {
-  // print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
+const restify = require("restify"),
+  restifyPlugins = require("restify-plugins"),
+  passport = require("passport"),
+  BearerStrategy = require("passport-azure-ad").BearerStrategy,
+  config = require("./config"),
+  authenticatedUserTokens = [],
+  serverPort = process.env.PORT || config.serverPort;
+
+const authenticationStrategy = new BearerStrategy(
+  config.credentials,
+  (token, done) => {
+    let currentUser = null;
+
+    let userToken = authenticatedUserTokens.find(user => {
+      currentUser = user;
+      user.sub === token.sub;
+    });
+
+    if (!userToken) {
+      authenticatedUserTokens.push(token);
+    }
+
+    return done(null, currentUser, token);
+  }
+);
+
+passport.use(authenticationStrategy);
+
+const server = restify.createServer({
+  name: "Azure Active Directroy with Node.js Demo"
 });
+server.use(restifyPlugins.authorizationParser());
+server.use(passport.initialize());
+server.use(passport.session());
+
+// Serve up public/ftp folder
+server.pre(serveStatic("public", { index: ["index.html", "index.htm"] }));
+
+server.listen(serverPort);
+// start server on the specified port and binding host
+// app.listen(appEnv.port, "0.0.0.0", function() {
+//   // print a message when the server starts listening
+//   console.log("server starting on " + appEnv.url);
+// });
---
+++ config.js
@@ -0,0 +1,10 @@
+const tenantName = "CSCPortal.onmicrosoft.com";
+const clientID = "fede1873-aa95-4256-b554-1aa5a89e26ef";
+const serverPort = 3000;
+
+module.exports.serverPort = serverPort;
+
+module.exports.credentials = {
+  identityMetadata: `https://login.microsoftonline.com/${tenantName}.onmicrosoft.com/.well-known/openid-configuration`,
+  clientID: clientID
+};
