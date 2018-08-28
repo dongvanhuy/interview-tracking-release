c4deefe92e19eb52fb32ab6a06fe0eab5ceb6e3f
VDC\thuynh63 <quocthang.it247@gmail.com>
2018-08-28 13:10:46 +0700

    no message

--- config.js
+++ config.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 const tenantName = "CSCPortal";
 const clientID = "fede1873-aa95-4256-b554-1aa5a89e26ef";
-=======
-const tenantName = "CSCPortal.onmicrosoft.com";
-const clientID = "9a29d40d-8291-46d0-ab97-4e650a71f363";
->>>>>>> c8b20dae7c33029a062001e3b99b3cb8bfa6ac7f
 const serverPort = 3000;
 
 module.exports.serverPort = serverPort;
