bbf4729b1da1423c9fe37779768857706b3c6908
VDC\hdong4 <huy.dong@hubcba.com>
2018-09-12 16:41:33 +0700

    huy build 12/9

--- public/service-worker.js
+++ public/service-worker.js
@@ -11,7 +11,7 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.5.0/workbox-sw.js");
 
 workbox.skipWaiting();
 workbox.clientsClaim();
@@ -24,7 +24,7 @@ workbox.clientsClaim();
 self.__precacheManifest = [
   {
     "url": "asset-manifest.json",
-    "revision": "03fe545573ecc1f991e3e9a4b831b9c7"
+    "revision": "cf9642f8b84826020f4f7a481a5d1f53"
   },
   {
     "url": "dxclogo192x192.png",
@@ -39,20 +39,52 @@ self.__precacheManifest = [
     "revision": "2c9781750fbcb16ebd475c7149e15d7d"
   },
   {
+    "url": "images/icons/icon-128x128.png",
+    "revision": "5cbdb95eba0b3dd4eb10e4c8e78a7be5"
+  },
+  {
+    "url": "images/icons/icon-144x144.png",
+    "revision": "4c1ea81a7a6ab663c8ba155ae0c0aad0"
+  },
+  {
+    "url": "images/icons/icon-152x152.png",
+    "revision": "462a84743116f4506d71692f5588d8a8"
+  },
+  {
+    "url": "images/icons/icon-192x192.png",
+    "revision": "a4089d3036cbd4b023cd12f36edbec07"
+  },
+  {
+    "url": "images/icons/icon-384x384.png",
+    "revision": "65d598def1c91aebe8e6c5169b95216b"
+  },
+  {
+    "url": "images/icons/icon-512x512.png",
+    "revision": "b186d159d59b3e399731dedcab1a215c"
+  },
+  {
+    "url": "images/icons/icon-72x72.png",
+    "revision": "dae20dda483d26c779bb9f77160e4d0f"
+  },
+  {
+    "url": "images/icons/icon-96x96.png",
+    "revision": "9a8ad6ec8b5a96c74330b2ca1c9020c1"
+  },
+  {
     "url": "index.html",
-    "revision": "0d98f17b7fc6e23916e9b00717d3cc41"
+    "revision": "b8d40e23f2a59b8f39cf0de5a305080a"
   },
   {
     "url": "manifest.json",
-    "revision": "b0a14f49e2067a7a9a7c9ceba84de8c3"
+    "revision": "190c5a55e0ba49b3bbaa0e20d2e7f471"
   },
   {
-    "url": "static/css/main.11e31fe2.css",
-    "revision": "8cf89dc02444fd3440d6405a73f341df"
+    "url": "static/css/main.342b5aac.css",
+    "revision": "cd7c2d322eae4aec4087ba8b4c015140"
   },
   {
-    "url": "static/js/main.594d7d24.js",
-    "revision": "df76c9d222a42a6ad3f2f1a91d6fdf30"
+    "url": "static/js/main.18dc3921.js",
+    "revision": "04317cfc34053f9f7d0c7c4e218b6023"
   },
   {
     "url": "static/media/dxcLogo.8500f535.svg",
@@ -110,4 +142,4 @@ workbox.routing.registerNavigationRoute("/index.html");
 
 workbox.routing.registerRoute(/https:\/\/dxc-interview-tracking-api-release.azurewebsites.net\/api\/(.*)/, workbox.strategies.networkFirst(), 'GET');
 workbox.routing.registerRoute(/https:\/\/dxc-interview-tracking-api-release.azurewebsites.net\/api\/(.*)/, workbox.strategies.staleWhileRevalidate({ plugins: [new workbox.cacheableResponse.Plugin({"statuses":[0,200]})] }), 'GET');
-workbox.routing.registerRoute(/https:\/\/dxc-interview-tracking-api-release.azurewebsites.net\/api\/(.*)/, workbox.strategies.cacheFirst({ cacheName: "my-api-cache", plugins: [new workbox.expiration.Plugin({"maxEntries":50,"maxAgeSeconds":2592000,"purgeOnQuotaError":false}), new workbox.cacheableResponse.Plugin({"statuses":[0,200]})] }), 'GET');
+workbox.routing.registerRoute(/https:\/\/dxc-interview-tracking-api-release.azurewebsites.net\/api\/(.*)/, workbox.strategies.cacheFirst({ "cacheName":"my-api-cache", plugins: [new workbox.expiration.Plugin({"maxEntries":50,"maxAgeSeconds":2592000,"purgeOnQuotaError":false}), new workbox.cacheableResponse.Plugin({"statuses":[0,200]})] }), 'GET');
