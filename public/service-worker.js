f2e09a4fb241d198ede1a680492b3c64107cd23d
VDC\hdong4 <huy.dong@hubcba.com>
2018-09-26 08:03:09 +0700

    huy build 3

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,161 @@
-"use strict";var precacheConfig=[["/index.html","090859cceb5703c6fa988bb5d84d2b8a"],["/static/css/main.e5a85c75.css","26ef71d3840749724d4b73cb7f77648c"],["/static/js/main.6f43fc2d.js","15d8608912eaf20c66f4a07680f677f2"],["/static/media/GTWalsheimProBold.de407daa.woff","de407daa6db34a1958fd38f71604e278"],["/static/media/GTWalsheimProRegular.7052fe6d.woff","7052fe6d98383402c64153edbfd406a9"],["/static/media/ProximaNova-Bold.72b6f2c9.otf","72b6f2c9990bd3d37b8013a59ed78902"],["/static/media/dxcLogo.8500f535.svg","8500f53521d6cbd1d3cb2253b0e5f888"],["/static/media/glyphicons-halflings-regular.448c34a5.woff2","448c34a56d699c29117adc64c43affeb"],["/static/media/glyphicons-halflings-regular.89889688.svg","89889688147bd7575d6327160d64e760"],["/static/media/glyphicons-halflings-regular.e18bbf61.ttf","e18bbf611f2a2e43afc071aa2f4e1512"],["/static/media/glyphicons-halflings-regular.f4769f9b.eot","f4769f9bdb7466be65088239c12046d1"],["/static/media/glyphicons-halflings-regular.fa277232.woff","fa2772327f55d8198301fdb8bcfc8158"],["/static/media/illustration.68e13737.png","68e13737d82e40acba0554b8abb08b16"],["/static/media/loading.1336a69b.svg","1336a69b3b60ec204ed4d6685c3dfdfb"]],cacheName="sw-precache-v3-sw-precache-webpack-plugin-"+(self.registration?self.registration.scope:""),ignoreUrlParametersMatching=[/^utm_/],addDirectoryIndex=function(e,t){var n=new URL(e);return"/"===n.pathname.slice(-1)&&(n.pathname+=t),n.toString()},cleanResponse=function(e){return e.redirected?("body"in e?Promise.resolve(e.body):e.blob()).then(function(t){return new Response(t,{headers:e.headers,status:e.status,statusText:e.statusText})}):Promise.resolve(e)},createCacheKey=function(e,t,n,a){var r=new URL(e);return a&&r.pathname.match(a)||(r.search+=(r.search?"&":"")+encodeURIComponent(t)+"="+encodeURIComponent(n)),r.toString()},isPathWhitelisted=function(e,t){if(0===e.length)return!0;var n=new URL(t).pathname;return e.some(function(e){return n.match(e)})},stripIgnoredUrlParameters=function(e,t){var n=new URL(e);return n.hash="",n.search=n.search.slice(1).split("&").map(function(e){return e.split("=")}).filter(function(e){return t.every(function(t){return!t.test(e[0])})}).map(function(e){return e.join("=")}).join("&"),n.toString()},hashParamName="_sw-precache",urlsToCacheKeys=new Map(precacheConfig.map(function(e){var t=e[0],n=e[1],a=new URL(t,self.location),r=createCacheKey(a,hashParamName,n,/\.\w{8}\./);return[a.toString(),r]}));function setOfCachedUrls(e){return e.keys().then(function(e){return e.map(function(e){return e.url})}).then(function(e){return new Set(e)})}self.addEventListener("install",function(e){e.waitUntil(caches.open(cacheName).then(function(e){return setOfCachedUrls(e).then(function(t){return Promise.all(Array.from(urlsToCacheKeys.values()).map(function(n){if(!t.has(n)){var a=new Request(n,{credentials:"same-origin"});return fetch(a).then(function(t){if(!t.ok)throw new Error("Request for "+n+" returned a response with status "+t.status);return cleanResponse(t).then(function(t){return e.put(n,t)})})}}))})}).then(function(){return self.skipWaiting()}))}),self.addEventListener("activate",function(e){var t=new Set(urlsToCacheKeys.values());e.waitUntil(caches.open(cacheName).then(function(e){return e.keys().then(function(n){return Promise.all(n.map(function(n){if(!t.has(n.url))return e.delete(n)}))})}).then(function(){return self.clients.claim()}))}),self.addEventListener("fetch",function(e){if("GET"===e.request.method){var t,n=stripIgnoredUrlParameters(e.request.url,ignoreUrlParametersMatching),a="index.html";(t=urlsToCacheKeys.has(n))||(n=addDirectoryIndex(n,a),t=urlsToCacheKeys.has(n));var r="/index.html";!t&&"navigate"===e.request.mode&&isPathWhitelisted(["^(?!\\/__).*"],e.request.url)&&(n=new URL(r,self.location).toString(),t=urlsToCacheKeys.has(n)),t&&e.respondWith(caches.open(cacheName).then(function(e){return e.match(urlsToCacheKeys.get(n)).then(function(e){if(e)return e;throw Error("The cached response that was expected is missing.")})}).catch(function(t){return console.warn('Couldn\'t serve response for "%s" from cache: %O',e.request.url,t),fetch(e.request)}))}});
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js");
+
+workbox.skipWaiting();
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "asset-manifest.json",
+    "revision": "9154af852b5395b4dfc0b7506cd8f81e"
+  },
+  {
+    "url": "favicon-32x32.png",
+    "revision": "2c9781750fbcb16ebd475c7149e15d7d"
+  },
+  {
+    "url": "images/icon-128x128.png",
+    "revision": "5cbdb95eba0b3dd4eb10e4c8e78a7be5"
+  },
+  {
+    "url": "images/icon-144x144.png",
+    "revision": "4c1ea81a7a6ab663c8ba155ae0c0aad0"
+  },
+  {
+    "url": "images/icon-152x152.png",
+    "revision": "462a84743116f4506d71692f5588d8a8"
+  },
+  {
+    "url": "images/icon-192x192.png",
+    "revision": "a4089d3036cbd4b023cd12f36edbec07"
+  },
+  {
+    "url": "images/icon-384x384.png",
+    "revision": "65d598def1c91aebe8e6c5169b95216b"
+  },
+  {
+    "url": "images/icon-512x512.png",
+    "revision": "b186d159d59b3e399731dedcab1a215c"
+  },
+  {
+    "url": "images/icon-72x72.png",
+    "revision": "dae20dda483d26c779bb9f77160e4d0f"
+  },
+  {
+    "url": "images/icon-96x96.png",
+    "revision": "9a8ad6ec8b5a96c74330b2ca1c9020c1"
+  },
+  {
+    "url": "index.html",
+    "revision": "a2e8531f094317920a73ea2e52a286b6"
+  },
+  {
+    "url": "ios_splash.js",
+    "revision": "77023cc5a35c66a303853d8e50c62f89"
+  },
+  {
+    "url": "ios/ios-appicon-120-120.png",
+    "revision": "21875d18184365b5e8abc187247a821d"
+  },
+  {
+    "url": "ios/ios-appicon-152-152.png",
+    "revision": "60592a5df1acf8acdc22af31567bf0b4"
+  },
+  {
+    "url": "ios/ios-appicon-180-180.png",
+    "revision": "39ab90e22e071c44887cb2ecf8960f6b"
+  },
+  {
+    "url": "ios/ios-appicon-76-76.png",
+    "revision": "2dc498821b175890844a7e9f19bd571a"
+  },
+  {
+    "url": "ios/ios-launchimage-1242-2208.png",
+    "revision": "44a10fb4d124f88f9a6b63f700f916f0"
+  },
+  {
+    "url": "manifest.json",
+    "revision": "8e290249612432f51203f428c9097a9b"
+  },
+  {
+    "url": "static/css/main.e5a85c75.css",
+    "revision": "26ef71d3840749724d4b73cb7f77648c"
+  },
+  {
+    "url": "static/js/main.6f43fc2d.js",
+    "revision": "15d8608912eaf20c66f4a07680f677f2"
+  },
+  {
+    "url": "static/media/dxcLogo.8500f535.svg",
+    "revision": "8500f53521d6cbd1d3cb2253b0e5f888"
+  },
+  {
+    "url": "static/media/glyphicons-halflings-regular.448c34a5.woff2",
+    "revision": "448c34a56d699c29117adc64c43affeb"
+  },
+  {
+    "url": "static/media/glyphicons-halflings-regular.89889688.svg",
+    "revision": "89889688147bd7575d6327160d64e760"
+  },
+  {
+    "url": "static/media/glyphicons-halflings-regular.e18bbf61.ttf",
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512"
+  },
+  {
+    "url": "static/media/glyphicons-halflings-regular.f4769f9b.eot",
+    "revision": "f4769f9bdb7466be65088239c12046d1"
+  },
+  {
+    "url": "static/media/glyphicons-halflings-regular.fa277232.woff",
+    "revision": "fa2772327f55d8198301fdb8bcfc8158"
+  },
+  {
+    "url": "static/media/GTWalsheimProBold.de407daa.woff",
+    "revision": "de407daa6db34a1958fd38f71604e278"
+  },
+  {
+    "url": "static/media/GTWalsheimProRegular.7052fe6d.woff",
+    "revision": "7052fe6d98383402c64153edbfd406a9"
+  },
+  {
+    "url": "static/media/illustration.68e13737.png",
+    "revision": "68e13737d82e40acba0554b8abb08b16"
+  },
+  {
+    "url": "static/media/loading.1336a69b.svg",
+    "revision": "1336a69b3b60ec204ed4d6685c3dfdfb"
+  },
+  {
+    "url": "static/media/ProximaNova-Bold.72b6f2c9.otf",
+    "revision": "72b6f2c9990bd3d37b8013a59ed78902"
+  },
+  {
+    "url": "styles/index.css",
+    "revision": "664a9bd3b2ac169293391ea33b132bd6"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/index.html");
+
+workbox.routing.registerRoute(/https:\/\/dxc-interview-tracking-api-release.azurewebsites.net\/api\/(.*)/, workbox.strategies.networkFirst(), 'GET');
+workbox.routing.registerRoute(/https:\/\/dxc-interview-tracking-api-release.azurewebsites.net\/api\/(.*)/, workbox.strategies.staleWhileRevalidate({ plugins: [new workbox.cacheableResponse.Plugin({"statuses":[0,200]})] }), 'GET');
+workbox.routing.registerRoute(/https:\/\/dxc-interview-tracking-api-release.azurewebsites.net\/api\/(.*)/, workbox.strategies.cacheFirst({ "cacheName":"my-api-cache", plugins: [new workbox.expiration.Plugin({"maxEntries":50,"maxAgeSeconds":2592000,"purgeOnQuotaError":false}), new workbox.cacheableResponse.Plugin({"statuses":[0,200]})] }), 'GET');
