235988683e00a6c280b64d3d1079a62ab7cc4661
VDC\hdong4 <huy.dong@hubcba.com>
2018-09-28 13:59:34 +0700

    huybuild 5

--- public/service-worker.js
+++ public/service-worker.js
@@ -24,7 +24,7 @@ workbox.clientsClaim();
 self.__precacheManifest = [
   {
     "url": "asset-manifest.json",
-    "revision": "f3562ec39faaa79f25d9b1179abcfd24"
+    "revision": "f5e595645a6b41b2da3893455f11a26f"
   },
   {
     "url": "favicon-32x32.png",
@@ -64,7 +64,7 @@ self.__precacheManifest = [
   },
   {
     "url": "index.html",
-    "revision": "ce2e72afa1be393e6192df80fce3bddd"
+    "revision": "9292e8ee400ca849e79fbb89e5c4464a"
   },
   {
     "url": "ios_splash.js",
@@ -99,8 +99,8 @@ self.__precacheManifest = [
     "revision": "e29c37b8dfb800f378862f44f5bca9d5"
   },
   {
-    "url": "static/js/main.febc1cac.js",
-    "revision": "87a5bcc30b88c4cbca617fbc808ea014"
+    "url": "static/js/main.c51b9ca1.js",
+    "revision": "3c6f9d83b61dd66449d435b23ae54e2e"
   },
   {
     "url": "static/media/delete.2239c602.svg",
